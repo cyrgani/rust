6ab26238c4f8af925b218bc082fc6c4cc76f55e3
cyrgani <ansgar.w.zielke@gmail.com>
2026-01-08 11:41:16 +0000

    wip

--- compiler/rustc_builtin_macros/src/deriving/cmp/eq.rs
+++ compiler/rustc_builtin_macros/src/deriving/cmp/eq.rs
@@ -1,7 +1,7 @@
 use rustc_ast::{self as ast, MetaItem, Safety};
 use rustc_data_structures::fx::FxHashSet;
 use rustc_expand::base::{Annotatable, ExtCtxt};
-use rustc_span::{Span, sym};
+use rustc_span::{Ident, Span, kw, sym};
 use thin_vec::{ThinVec, thin_vec};
 
 use crate::deriving::generic::ty::*;
@@ -17,6 +17,7 @@ pub(crate) fn expand_deriving_eq(
     is_const: bool,
 ) {
     let span = cx.with_def_site_ctxt(span);
+    let mut new_stmts = ThinVec::new();
 
     let trait_def = TraitDef {
         span,
@@ -38,7 +39,7 @@ pub(crate) fn expand_deriving_eq(
             ],
             fieldless_variants_strategy: FieldlessVariantsStrategy::Unify,
             combine_substructure: combine_substructure(Box::new(|a, b, c| {
-                cs_total_eq_assert(a, b, c)
+                cs_total_eq_assert(a, b, c, &mut new_stmts)
             })),
         }],
         associated_types: Vec::new(),
@@ -47,13 +48,73 @@ pub(crate) fn expand_deriving_eq(
         safety: Safety::Default,
         document: true,
     };
-    trait_def.expand_ext(cx, mitem, item, push, true)
+    trait_def.expand_ext(cx, mitem, item, push, true);
+    let bound = cx.trait_bound(
+        cx.path_global(
+            span,
+            vec![
+                Ident::new(sym::core, span),
+                Ident::new(sym::cmp, span),
+                Ident::new(sym::Eq, span),
+            ],
+        ),
+        is_const,
+    );
+    let body = Some(cx.block(span, new_stmts));
+    let mut generics =
+        item.clone().expect_item().opt_generics().cloned().unwrap_or(ast::Generics::default());
+    generics.params = generics
+        .params
+        .into_iter()
+        .map(|mut param| {
+            if matches!(param.kind, ast::GenericParamKind::Type { .. }) {
+                param.bounds.push(bound.clone());
+                param.kind = ast::GenericParamKind::Type { default: None }
+            }
+            param
+        })
+        .collect();
+    let mut header = ast::FnHeader::default();
+    header.constness = if is_const { ast::Const::Yes(span) } else { ast::Const::No };
+    let kind = ast::ItemKind::Fn(Box::new(ast::Fn {
+        defaultness: ast::Defaultness::Final,
+        sig: ast::FnSig {
+            header,
+            decl: cx.fn_decl(ThinVec::new(), ast::FnRetTy::Default(span)),
+            span,
+        },
+        ident: Ident::new(sym::assert_receiver_is_total_eq, span),
+        generics,
+        contract: None,
+        body,
+        define_opaque: None,
+        eii_impls: ThinVec::new(),
+    }));
+    let item = cx.item(
+        span,
+        thin_vec![
+            cx.attr_nested_word(sym::doc, sym::hidden, span),
+            cx.attr_nested_word(sym::coverage, sym::off, span)
+        ],
+        kind,
+    );
+    let stmt = cx.stmt_item(span, item);
+    let block = ast::ConstItemRhs::Body(cx.expr_block(cx.block(span, thin_vec![stmt])));
+
+    let anon_constant = cx.item_const(
+        span,
+        Ident::new(kw::Underscore, span),
+        cx.ty(span, ast::TyKind::Tup(ThinVec::new())),
+        block,
+    );
+    push(Annotatable::Item(anon_constant));
 }
 
 fn cs_total_eq_assert(
     cx: &ExtCtxt<'_>,
     trait_span: Span,
     substr: &Substructure<'_>,
+    new_stmts: &mut ThinVec<ast::Stmt>,
 ) -> BlockOrExpr {
     let mut stmts = ThinVec::new();
     let mut seen_type_names = FxHashSet::default();
@@ -68,9 +129,16 @@ fn cs_total_eq_assert(
                 // Already produced an assertion for this type.
             } else {
                 // let _: AssertParamIsEq<FieldTy>;
-                super::assert_ty_bounds(
+                /*super::assert_ty_bounds(
                     cx,
                     &mut stmts,
+                    field.ty.clone(),
+                    field.span,
+                    &[sym::cmp, sym::AssertParamIsEq],
+                );*/
+                super::assert_ty_bounds(
+                    cx,
+                    new_stmts,
                     field.ty.clone(),
                     field.span,
                     &[sym::cmp, sym::AssertParamIsEq],
